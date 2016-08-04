0543dcd978a4d1feed2312fa71d94e3517f95baa
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-04 09:55:30 -0500

    feat: User can navigate to new book page

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('author', { path: '/authors' }, function() {
     this.route('new');
-    this.route('detail', { path: '/:id/books' });
+    this.route('detail', { path: '/:id/books' }, function() {
+      this.route('add-book', { path: '/add' });
+    });
   });
   this.route('books');
 });
--- tests/acceptance/books-test.js
+++ tests/acceptance/books-test.js
@@ -8,7 +8,7 @@ test('user can see details for an author', function(assert) {
   visit('/authors/1/books');
 
   andThen(function() {
-    assert.equal(currentRouteName(), 'author.detail');
+    assert.equal(currentRouteName(), 'author.detail.index');
 
     const author = server.db.authors.find(1);
     assert.equal(findWithAssert('.author-name').text().trim(),
@@ -17,11 +17,26 @@ test('user can see details for an author', function(assert) {
 });
 
 test('user can navigate back to all authors page', function (assert) {
-  server.create('author')
+  server.create('author');
   visit('/authors/1/books');
   click('.back');
 
   andThen(function() {
     assert.equal(currentURL(), '/authors');
+  });
+});
+
+test('user can navigate to a new book form from detail page', function(assert) {
+  // Setup (Plan)
+  server.create('author');
+
+  // Action (Build)
+  visit('/authors/1/books');
+  click('.new-book');
+
+  // Assertion (React)
+  andThen(function() {
+    assert.equal(currentURL(), '/authors/1/books/add');
+    assert.equal(currentRouteName(), 'author.detail.add-book');
   });
 });
