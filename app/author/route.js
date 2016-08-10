2ae0446ff9615489a5d8f4ba83abb860616106c9
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-10 11:53:31 -0500

    Only logged in users can see authors page

--- app/author/route.js
+++ app/author/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
+
+  beforeModel() {
+    // Check that user is authenticated!
+    if (!this.get('session.isAuthenticated')) {
+      alert('You have to be logged in!');
+
+      this.transitionTo('login');
+    }
+  },
 });
