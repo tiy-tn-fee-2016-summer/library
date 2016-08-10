d52da0929d8fe4ba63577769b8a46336fca02d19
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-10 12:05:33 -0500

    Authorize requests to the API with the logged in token

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,9 @@
 import DS from 'ember-data';
 import config from 'library/config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:token',
   host: config.DS.host,
   namespace: config.DS.namespace,
 
